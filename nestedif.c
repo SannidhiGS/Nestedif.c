60e840eeab3416633a0310ff916e0f9745eb5174
Sannidhi G S <128506865+SannidhiGS@users.noreply.github.com>
2024-02-06 21:12:16 +0530

    Create nestedif.c
    If statement within a if statement is called nestedif.

---
+++ nestedif.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+   int x = 10;
+   int y = 5;
+
+   if (x > y) {
+      printf("x is greater than y\n");
+
+      if (x > 0) {
+         printf("x is positive\n");
+      }
+   }
+
+   return 0;
+}
